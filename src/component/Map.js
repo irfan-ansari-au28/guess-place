23d933bca3992083a9968d96e96461b4479eec38
phoreka <irfan398649@gmail.com>
2023-03-11 11:52:58 +0530

    add distance logic

--- src/component/Map.js
+++ src/component/Map.js
@@ -6,13 +6,82 @@ import {
   Graticule,
   Marker,
 } from "react-simple-maps";
+import { getDistanceFromCoords } from "./utils/getDistFromCoOrds";
+
+const cities = [
+  {
+    name: "Amsterdam",
+    position: {
+      lat: 52.370216,
+      lng: 4.895168,
+    },
+  },
+  {
+    name: "Rome",
+    position: {
+      lat: 41.902783,
+      lng: 12.496366,
+    },
+  },
+  {
+    name: "Helsenki",
+    position: {
+      lat: 60.169856,
+      lng: 24.938379,
+    },
+  },
+  {
+    name: "Stockholm",
+    position: {
+      lat: 59.329323,
+      lng: 18.068581,
+    },
+  },
+  {
+    name: "London",
+    position: {
+      lat: 51.507351,
+      lng: -0.127758,
+    },
+  },
+  {
+    name: "Oslo",
+    position: {
+      lat: 59.913869,
+      lng: 10.752245,
+    },
+  },
+  {
+    name: "Paris",
+    position: {
+      lat: 48.856614,
+      lng: 2.352222,
+    },
+  },
+  {
+    name: "Wien",
+    position: {
+      lat: 48.208174,
+      lng: 16.373819,
+    },
+  },
+  {
+    name: "Budapest",
+    position: {
+      lat: 47.497912,
+      lng: 19.040235,
+    },
+  },
+];
 
 const geoUrl =
   "https://raw.githubusercontent.com/deldersveld/topojson/master/continents/europe.json";
 
 const MapC = () => {
   const [place, setPlace] = useState(null);
-  const [coOrds, setCoOrds] = useState(null);
+  const [coOrds, setCoOrds] = useState([[[11.08, 47.39]]]);
+  const [score, setScore] = useState(15000);
+  const [dist, setDist] = useState(0);
   // let randomIdx;
 
   // handle click on Map
@@ -22,12 +91,27 @@ const MapC = () => {
     // Set coOrdinates on Click
     setCoOrds(geo.geometry.coordinates[0]);
     console.log("geoUnit", place);
-    console.log(geo);
+    // console.log(geo);
 
     // if (coOrds[1] || coOrds[0]) {
     //   randomIdx = Math.floor(Math.random() * (coOrds[1] || coOrds[0].length));
     // }
     // console.log("idx", randomIdx);
+    // Get Distance
+    console.log(coOrds);
+    const dist = getDistanceFromCoords(
+      coOrds[0][0],
+      coOrds[0][1],
+      -7.24,
+      55.06
+    );
+    // set Distance
+    setDist(dist);
+    console.log("dist", dist);
+
+    if (dist < 250) {
+      alert("found");
+    }
   };
   return (
     <ComposableMap
@@ -58,6 +142,13 @@ const MapC = () => {
           <circle r={8} fill="#F53" />
         </Marker>
       )}
+      {
+        <Marker coordinates={[-16.01, 67.53]}>
+          <text textAnchor="middle" fill="blue">
+            {`Score : ${score}`}
+          </text>
+        </Marker>
+      }
     </ComposableMap>
   );
 };
---
+++ src/component/utils/getDistFromCoOrds.js
@@ -0,0 +1,22 @@
+export function getDistanceFromCoords(lat1, lng1, lat2, lng2) {
+  const earthRadius = 6371; // km
+
+  const dLat = degToRad(lat2 - lat1);
+  const dLng = degToRad(lng2 - lng1);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(degToRad(lat1)) *
+      Math.cos(degToRad(lat2)) *
+      Math.sin(dLng / 2) *
+      Math.sin(dLng / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const distance = earthRadius * c;
+  return distance;
+}
+
+function degToRad(degrees) {
+  return degrees * (Math.PI / 180);
+}
